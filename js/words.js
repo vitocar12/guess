5821b68ba583ee825d7010b80167b8b79b6db129
vitocar12 <145636400+vitocar12@users.noreply.github.com>
2024-11-25 13:29:10 +0700

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,127 @@
+let wordList = [
+    {
+        word: "sukuna",
+        hint: "karakter terkuat di jjk"
+    },
+    {
+        word: "onepiece",
+        hint: "manga dengan pendapatan 500jt copy"
+    },
+    {
+        word: "gojo",
+        hint: "mc di jujutsu kaisen"
+    },
+    {
+        word: "yuji",
+        hint: "side karakter di jjk"
+    },
+    {
+        word: "rusdi",
+        hint: "sahabat mas faiz"
+    },
+    {
+        word: "oda",
+        hint: "mangaka one piece"
+    },
+    {
+        word: "gurenge",
+        hint: "nama lagu opening kimetsu no yaiba s1"
+    },
+    {
+        word: "gegeakutani",
+        hint: "nama mangaka jujutsu kaisen"
+    },
+    {
+        word: "aimer",
+        hint: "nama penyanyi lagu zankyou sanka"
+    },
+    {
+        word: "miku",
+        hint: "waifu terbaik sepanjang masa"
+    },
+    {
+        word: "ufotable",
+        hint: "studio yang menggarap kimetsu no yaiba"
+    },
+    {
+        word: "marinford",
+        hint: "ace mati di arc"
+    },
+    {
+        word: "yorichi",
+        hint: "pilar terkuat sepanjang masa di kny"
+    },
+    {
+        word: "mappa",
+        hint: "jujutsu kaisen di garap di studio?"
+    },
+    {
+        word: "zoro",
+        hint: "nama kru pertama di mugiwara"
+    },
+    {
+        word: "nakime",
+        hint: "siapa sih nama upper moon ke 4"
+    },
+    {
+        word: "goingmerry",
+        hint: "nama kapal pertama kru mugiwara"  
+    },
+    {
+        word: "akainu",
+        hint: "admiral terkuat di one piece"  
+    },
+    {
+        word: "rengoku",
+        hint: "di movie kimetsu mugen train siapakah hashira yang mati"
+    },
+    {
+        word: "empat",
+        hint: "ada berapa sesoan anime haikyuu"
+    },
+    {
+        word: "hajimekashimo",
+        hint: "nama karakter yang melawan sukuna sesaat sudah mengalahkan gojo"
+    },
+    {
+        word: "aimer",
+        hint: "lagu zankyou sanka di nyanyikan oleh"
+    },
+    {
+        word:"boruto",
+        hint:"siapakah anak raikage" 
+    },
+    {
+        word:"saitama",
+        hint:"strong karakter from one puch man"
+    },
+    {
+        word:"takemichi",
+        hint:"mc di tokyo revengers"
+    },
+    {
+        word:"garp",
+        hint:"nama kakek luffy"
+    },
+    {
+        word:"yuno",
+        hint:"mc di black clover"    
+    },
+    {
+        word:"okarun",
+        hint:"karaker utama di anime dandandan"
+
+    },
+    {
+        word:"vegapunk",
+        hint:"ilmuan terhebat di one piece"
+    }
+    
+        
+
+
+    
+    
+    
+
+]
